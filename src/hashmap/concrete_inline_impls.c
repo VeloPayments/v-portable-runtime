d1a5885d705be0fdecaad71ca8e29d279516427a
Justin Handville <nanolith@gmail.com>
2021-02-02 00:20:35 -0500

    Added debug support for inline functions in hashmap.

---
+++ src/hashmap/concrete_inline_impls.c
@@ -0,0 +1,12 @@
+/**
+ * \file hashmap/concrete_inline_impls.c
+ *
+ * \brief Provide concrete implementations for inline functions for debug-time
+ * linkage.
+ *
+ * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ */
+
+#define VPR_HASHMAP_CONCRETE_IMPLEMENTATION
+
+#include <vpr/hashmap.h>
