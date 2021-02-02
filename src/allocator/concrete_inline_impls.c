9f387e04300f15923455e7a6548b32355bfd28b2
Justin Handville <nanolith@gmail.com>
2021-02-01 21:45:01 -0500

    Added debug support for inline functions in allocator.

---
+++ src/allocator/concrete_inline_impls.c
@@ -0,0 +1,12 @@
+/**
+ * \file allocator/concrete_inline_impls.c
+ *
+ * \brief Provide concrete implementations for inline functions for debug-time
+ * linkage.
+ *
+ * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ */
+
+#define VPR_ALLOCATOR_CONCRETE_IMPLEMENTATION
+
+#include <vpr/dynamic_array.h>
