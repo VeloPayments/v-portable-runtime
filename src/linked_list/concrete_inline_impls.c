d7fd1b15fe05ac790cdafafd6e98364f97b17f77
Justin Handville <nanolith@gmail.com>
2021-02-02 00:25:19 -0500

    Added debug support for inline functions in linked_list.

---
+++ src/linked_list/concrete_inline_impls.c
@@ -0,0 +1,12 @@
+/**
+ * \file linked_list/concrete_inline_impls.c
+ *
+ * \brief Provide concrete implementations for inline functions for debug-time
+ * linkage.
+ *
+ * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ */
+
+#define VPR_LINKED_LIST_CONCRETE_IMPLEMENTATION
+
+#include <vpr/linked_list.h>
