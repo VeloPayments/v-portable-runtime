aff1eaedd74bb909c5d418b1976564fb99bb1d61
Justin Handville <nanolith@gmail.com>
2021-02-01 21:58:54 -0500

    Added debug support for inline functions in doubly_linked_list.

---
+++ src/doubly_linked_list/concrete_inline_impls.c
@@ -0,0 +1,12 @@
+/**
+ * \file doubly_linked_list/concrete_inline_impls.c
+ *
+ * \brief Provide concrete implementations for inline functions for debug-time
+ * linkage.
+ *
+ * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ */
+
+#define VPR_DOUBLY_LINKED_LIST_CONCRETE_IMPLEMENTATION
+
+#include <vpr/doubly_linked_list.h>
