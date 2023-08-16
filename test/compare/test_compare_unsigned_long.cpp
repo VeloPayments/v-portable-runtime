d1e1c964f3aa474fafc1bb3cdfae839a910087ce
Justin Handville <nanolith@gmail.com>
2023-08-15 21:51:24 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_unsigned_long.cpp
+++ test/compare/test_compare_unsigned_long.cpp
@@ -3,47 +3,47 @@
  *
  * Unit tests for compare_unsigned_long.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_unsigned_long);
 
 /**
  * Test that comparing two unsigned longs that are equal results in 0.
  */
-TEST(compare_unsigned_long, equality)
+TEST(equality)
 {
     const unsigned long X = 17;
     const unsigned long Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(unsigned long)));
-    EXPECT_EQ(0, compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 == compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_unsigned_long, greater_than)
+TEST(greater_than)
 {
     const unsigned long X = 17;
     const unsigned long Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(unsigned long)));
-    EXPECT_LT(0, compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 < compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_unsigned_long, less_than)
+TEST(less_than)
 {
     const unsigned long X = 17;
     const unsigned long Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(unsigned long)));
-    EXPECT_GT(0, compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(unsigned long)));
+    TEST_EXPECT(0 > compare_unsigned_long(&X, &Y, sizeof(unsigned long)));
 }
-#endif
