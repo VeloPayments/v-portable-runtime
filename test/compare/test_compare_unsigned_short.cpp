6578d52920c81bc601f856d2302164c0a873bf26
Justin Handville <nanolith@gmail.com>
2023-08-15 22:08:36 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_unsigned_short.cpp
+++ test/compare/test_compare_unsigned_short.cpp
@@ -3,47 +3,47 @@
  *
  * Unit tests for compare_unsigned_short.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_unsigned_short);
 
 /**
  * Test that comparing two unsigned shorts that are equal results in 0.
  */
-TEST(compare_unsigned_short, equality)
+TEST(equality)
 {
     const unsigned short X = 17;
     const unsigned short Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(unsigned short)));
-    EXPECT_EQ(0, compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 == compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_unsigned_short, greater_than)
+TEST(greater_than)
 {
     const unsigned short X = 17;
     const unsigned short Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(unsigned short)));
-    EXPECT_LT(0, compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 < compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_unsigned_short, less_than)
+TEST(less_than)
 {
     const unsigned short X = 17;
     const unsigned short Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(unsigned short)));
-    EXPECT_GT(0, compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(unsigned short)));
+    TEST_EXPECT(0 > compare_unsigned_short(&X, &Y, sizeof(unsigned short)));
 }
-#endif
