4c3d6da80dcc875526d1581931729d746498b768
Justin Handville <nanolith@gmail.com>
2023-08-15 20:55:30 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_double.cpp
+++ test/compare/test_compare_double.cpp
@@ -3,47 +3,47 @@
  *
  * Unit tests for compare_double.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_double);
 
 /**
  * Test that comparing two long longs that are equal results in 0.
  */
-TEST(compare_double, equality)
+TEST(equality)
 {
     const double X = 17.0;
     const double Y = 17.0;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(double)));
-    EXPECT_EQ(0, compare_double(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 == compare_double(&X, &Y, sizeof(double)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_double, greater_than)
+TEST(greater_than)
 {
     const double X = 17.0;
     const double Y = 14.0;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(double)));
-    EXPECT_LT(0, compare_double(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 < compare_double(&X, &Y, sizeof(double)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_double, less_than)
+TEST(less_than)
 {
     const double X = 17.0;
     const double Y = 19.0;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(double)));
-    EXPECT_GT(0, compare_double(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(double)));
+    TEST_EXPECT(0 > compare_double(&X, &Y, sizeof(double)));
 }
-#endif
