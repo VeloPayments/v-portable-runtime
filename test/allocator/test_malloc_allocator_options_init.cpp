d569e827d64448c76fc665f8080ff24496b33409
Justin Handville <nanolith@gmail.com>
2023-08-15 20:08:35 -0400

    Converted unit tests to minunit.

--- test/allocator/test_malloc_allocator_options_init.cpp
+++ test/allocator/test_malloc_allocator_options_init.cpp
@@ -3,31 +3,31 @@
  *
  * Unit tests for malloc_allocator_options_init.
  *
- * \copyright 2017-2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/allocator.h>
 #include <vpr/allocator/malloc_allocator.h>
+#include <minunit/minunit.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(malloc_allocator_options_init_test);
 
 /**
  * There's not much we can test here.  This function should be manually
  * reviewed.  We can at least test that some fields are set, and that the
  * context is null.
  */
-TEST(malloc_allocator_options_init_test, initTest)
+TEST(initTest)
 {
     allocator_options_t options;
 
     malloc_allocator_options_init(&options);
 
-    EXPECT_NE(nullptr, (void*)options.allocator_allocate);
-    EXPECT_NE(nullptr, (void*)options.allocator_release);
-    EXPECT_NE(nullptr, (void*)options.allocator_reallocate);
-    EXPECT_NE(nullptr, (void*)options.allocator_control);
-    EXPECT_EQ(0, options.context);
+    TEST_EXPECT(nullptr != (void*)options.allocator_allocate);
+    TEST_EXPECT(nullptr != (void*)options.allocator_release);
+    TEST_EXPECT(nullptr != (void*)options.allocator_reallocate);
+    TEST_EXPECT(nullptr != (void*)options.allocator_control);
+    TEST_EXPECT(0 == options.context);
 
     dispose(allocator_options_disposable_handle(&options));
 }
@@ -35,15 +35,15 @@ TEST(malloc_allocator_options_init_test, initTest)
 /**
  * There are no valid control knobs for malloc allocator.
  */
-TEST(malloc_allocator_options_init_test, allocator_control)
+TEST(allocator_control)
 {
     allocator_options_t options;
 
     malloc_allocator_options_init(&options);
 
-    EXPECT_EQ(VPR_ERROR_ALLOCATOR_CONTROL_INVALID_KEY,
-        allocator_control(&options, 1, nullptr));
+    TEST_EXPECT(
+        VPR_ERROR_ALLOCATOR_CONTROL_INVALID_KEY
+            == allocator_control(&options, 1, nullptr));
 
     dispose(allocator_options_disposable_handle(&options));
 }
-#endif
