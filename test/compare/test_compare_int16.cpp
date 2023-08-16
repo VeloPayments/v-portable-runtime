aa74489819e2f458c0beeec7ac037f3c673b42cb
Justin Handville <nanolith@gmail.com>
2023-08-15 21:03:08 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_int16.cpp
+++ test/compare/test_compare_int16.cpp
@@ -3,46 +3,47 @@
  *
  * Unit tests for compare_int16.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_int16);
+
 /**
  * Test that comparing two int16_t values that are equal results in 0.
  */
-TEST(compare_int16, equality)
+TEST(equality)
 {
     const int16_t X = 17;
     const int16_t Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(int16_t)));
-    EXPECT_EQ(0, compare_int16(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 == compare_int16(&X, &Y, sizeof(int16_t)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_int16, greater_than)
+TEST(greater_than)
 {
     const int16_t X = 17;
     const int16_t Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(int16_t)));
-    EXPECT_LT(0, compare_int16(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 < compare_int16(&X, &Y, sizeof(int16_t)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_int16, less_than)
+TEST(less_than)
 {
     const int16_t X = 17;
     const int16_t Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(int16_t)));
-    EXPECT_GT(0, compare_int16(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(int16_t)));
+    TEST_EXPECT(0 > compare_int16(&X, &Y, sizeof(int16_t)));
 }
-#endif
