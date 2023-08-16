f13ae0f40632a950c33e3034c8ca385ae47e0766
Justin Handville <nanolith@gmail.com>
2023-08-15 21:34:02 -0400

    Convert unit tests to minunit.

--- test/compare/test_compare_short.cpp
+++ test/compare/test_compare_short.cpp
@@ -3,46 +3,47 @@
  *
  * Unit tests for compare_short.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_short);
+
 /**
  * Test that comparing two shorts that are equal results in 0.
  */
-TEST(compare_short, equality)
+TEST(equality)
 {
     const short X = 17;
     const short Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(short)));
-    EXPECT_EQ(0, compare_short(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 == compare_short(&X, &Y, sizeof(short)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_short, greater_than)
+TEST(greater_than)
 {
     const short X = 17;
     const short Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(short)));
-    EXPECT_LT(0, compare_short(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 < compare_short(&X, &Y, sizeof(short)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_short, less_than)
+TEST(less_than)
 {
     const short X = 17;
     const short Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(short)));
-    EXPECT_GT(0, compare_short(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(short)));
+    TEST_EXPECT(0 > compare_short(&X, &Y, sizeof(short)));
 }
-#endif
