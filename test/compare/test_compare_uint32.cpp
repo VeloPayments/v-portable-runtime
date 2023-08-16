e75bd5d5c6f11bd10e960538ec91a7475a8cfa7f
Justin Handville <nanolith@gmail.com>
2023-08-15 21:39:00 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_uint32.cpp
+++ test/compare/test_compare_uint32.cpp
@@ -3,46 +3,47 @@
  *
  * Unit tests for compare_uint32.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_uint32);
+
 /**
  * Test that comparing two uint32_t values that are equal results in 0.
  */
-TEST(compare_uint32, equality)
+TEST(equality)
 {
     const uint32_t X = 17;
     const uint32_t Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(uint32_t)));
-    EXPECT_EQ(0, compare_uint32(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 == compare_uint32(&X, &Y, sizeof(uint32_t)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_uint32, greater_than)
+TEST(greater_than)
 {
     const uint32_t X = 17;
     const uint32_t Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(uint32_t)));
-    EXPECT_LT(0, compare_uint32(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 < compare_uint32(&X, &Y, sizeof(uint32_t)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_uint32, less_than)
+TEST(less_than)
 {
     const uint32_t X = 17;
     const uint32_t Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(uint32_t)));
-    EXPECT_GT(0, compare_uint32(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(uint32_t)));
+    TEST_EXPECT(0 > compare_uint32(&X, &Y, sizeof(uint32_t)));
 }
-#endif
