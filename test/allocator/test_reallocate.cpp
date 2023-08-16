14bc6c9faa77d4072829309459c983db4f124ef4
Justin Handville <nanolith@gmail.com>
2023-08-15 20:13:00 -0400

    Converted unit tests to minunit.

--- test/allocator/test_reallocate.cpp
+++ test/allocator/test_reallocate.cpp
@@ -3,21 +3,22 @@
  *
  * Unit tests for allocator.reallocate.
  *
- * \copyright 2017-2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/allocator.h>
-
-/* DISABLED GTEST */
-#if 0
+#include <minunit/minunit.h>
+#include <string.h>
 
 #include "mock_allocator.h"
+
+TEST_SUITE(allocator_reallocate);
 
 /**
  * Test that reallocate calls the allocator_reallocate method when it is
  * defined.
  */
-TEST(allocator_reallocate, reallocateOptionTest)
+TEST(reallocateOptionTest)
 {
     void* MEM = (void*)1234;
     const size_t OLD_SIZE = 99;
@@ -29,10 +30,10 @@ TEST(allocator_reallocate, reallocateOptionTest)
     mock_allocator_options_init(&options, true);
 
     //calling the mock returns null
-    EXPECT_EQ(nullptr, reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
+    TEST_EXPECT(nullptr == reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
 
     //the allocator_reallocate mock should have been called with our arguments
-    EXPECT_TRUE(
+    TEST_EXPECT(
         mock_allocator_reallocate_called(&options, MEM, OLD_SIZE, NEW_SIZE));
 
     //dispose options now that we're done
@@ -43,7 +44,7 @@ TEST(allocator_reallocate, reallocateOptionTest)
  * Test that we fall back to using allocate / release when allocate_reallocate
  * is not defined.
  */
-TEST(allocator_reallocate, noReallocateTest)
+TEST(noReallocateTest)
 {
     char old_buffer[200];
     char new_buffer[400];
@@ -65,23 +66,22 @@ TEST(allocator_reallocate, noReallocateTest)
     mock_allocator_allocate_retval(&options, ALLOC_RETVAL);
 
     //calling our method returns the ALLOC_RETVAL value.
-    EXPECT_EQ(ALLOC_RETVAL, reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
+    TEST_EXPECT(ALLOC_RETVAL == reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
 
     //the allocator_reallocate mock should NOT have been called.
-    EXPECT_FALSE(
-        mock_allocator_reallocate_called(&options));
+    TEST_EXPECT(!mock_allocator_reallocate_called(&options));
 
     //the allocator_allocate mock should have been called
-    EXPECT_TRUE(
+    TEST_EXPECT(
         mock_allocator_allocate_called(&options, NEW_SIZE));
 
     //the allocator_release mock should have been called
-    EXPECT_TRUE(
+    TEST_EXPECT(
         mock_allocator_release_called(&options, MEM));
 
     //OLD_SIZE bytes from the old buffer should have been copied to the
     //new buffers
-    EXPECT_EQ(0, memcmp(old_buffer, new_buffer, OLD_SIZE));
+    TEST_EXPECT(0 == memcmp(old_buffer, new_buffer, OLD_SIZE));
 
     //dispose options now that we're done
     dispose(allocator_options_disposable_handle(&options));
@@ -91,7 +91,7 @@ TEST(allocator_reallocate, noReallocateTest)
  * Test that we truncate when NEW_SIZE < OLD_SIZE and allocator_reallocate is
  * disabled.
  */
-TEST(allocator_reallocate, noReallocateTruncate)
+TEST(noReallocateTruncate)
 {
     char old_buffer[200];
     char new_buffer[100];
@@ -113,25 +113,23 @@ TEST(allocator_reallocate, noReallocateTruncate)
     mock_allocator_allocate_retval(&options, ALLOC_RETVAL);
 
     //calling our method returns the ALLOC_RETVAL value.
-    EXPECT_EQ(ALLOC_RETVAL, reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
+    TEST_EXPECT(ALLOC_RETVAL == reallocate(&options, MEM, OLD_SIZE, NEW_SIZE));
 
     //the allocator_reallocate mock should NOT have been called.
-    EXPECT_FALSE(
-        mock_allocator_reallocate_called(&options));
+    TEST_EXPECT(!mock_allocator_reallocate_called(&options));
 
     //the allocator_allocate mock should have been called
-    EXPECT_TRUE(
+    TEST_EXPECT(
         mock_allocator_allocate_called(&options, NEW_SIZE));
 
     //the allocator_release mock should have been called
-    EXPECT_TRUE(
+    TEST_EXPECT(
         mock_allocator_release_called(&options, MEM));
 
     //NEW_SIZE bytes from the old buffer should have been copied to the
     //new buffers
-    EXPECT_EQ(0, memcmp(old_buffer, new_buffer, NEW_SIZE));
+    TEST_EXPECT(0 == memcmp(old_buffer, new_buffer, NEW_SIZE));
 
     //dispose options now that we're done
     dispose(allocator_options_disposable_handle(&options));
 }
-#endif
