d111546cc89deee18dfd0eb8d9297e920828b92f
Justin Handville <nanolith@gmail.com>
2023-08-15 21:07:37 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_int64.cpp
+++ test/compare/test_compare_int64.cpp
@@ -3,46 +3,47 @@
  *
  * Unit tests for compare_int64.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_int64);
+
 /**
  * Test that comparing two int64_t values that are equal results in 0.
  */
-TEST(compare_int64, equality)
+TEST(equality)
 {
     const int64_t X = 17;
     const int64_t Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(int64_t)));
-    EXPECT_EQ(0, compare_int64(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 == compare_int64(&X, &Y, sizeof(int64_t)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_int64, greater_than)
+TEST(greater_than)
 {
     const int64_t X = 17;
     const int64_t Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(int64_t)));
-    EXPECT_LT(0, compare_int64(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 < compare_int64(&X, &Y, sizeof(int64_t)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_int64, less_than)
+TEST(less_than)
 {
     const int64_t X = 17;
     const int64_t Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(int64_t)));
-    EXPECT_GT(0, compare_int64(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(int64_t)));
+    TEST_EXPECT(0 > compare_int64(&X, &Y, sizeof(int64_t)));
 }
-#endif
