f08b4ca7eea3e90b0bddffe5aa729f01932e16a2
Justin Handville <nanolith@gmail.com>
2023-08-15 21:49:14 -0400

    Converted unit tests to minunit.

--- test/compare/test_compare_unsigned_int.cpp
+++ test/compare/test_compare_unsigned_int.cpp
@@ -3,46 +3,47 @@
  *
  * Unit tests for compare_unsigned_int.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <vpr/compare.h>
+#include <minunit/minunit.h>
+#include <string.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(compare_unsigned_int);
+
 /**
  * Test that comparing two unsigned ints that are equal results in 0.
  */
-TEST(compare_unsigned_int, equality)
+TEST(equality)
 {
     const unsigned int X = 17;
     const unsigned int Y = 17;
 
-    EXPECT_EQ(0, memcmp(&X, &Y, sizeof(unsigned int)));
-    EXPECT_EQ(0, compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 == memcmp(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 == compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
 }
 
 /**
  * Test that X > Y results in a return value that is greater than zero.
  */
-TEST(compare_unsigned_int, greater_than)
+TEST(greater_than)
 {
     const unsigned int X = 17;
     const unsigned int Y = 14;
 
-    EXPECT_LT(0, memcmp(&X, &Y, sizeof(unsigned int)));
-    EXPECT_LT(0, compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 < memcmp(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 < compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
 }
 
 /**
  * Test that X < Y results in a return value that is less than zero.
  */
-TEST(compare_unsigned_int, less_than)
+TEST(less_than)
 {
     const unsigned int X = 17;
     const unsigned int Y = 19;
 
-    EXPECT_GT(0, memcmp(&X, &Y, sizeof(unsigned int)));
-    EXPECT_GT(0, compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 > memcmp(&X, &Y, sizeof(unsigned int)));
+    TEST_EXPECT(0 > compare_unsigned_int(&X, &Y, sizeof(unsigned int)));
 }
-#endif
