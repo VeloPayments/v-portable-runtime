583d3bfaba641018291d7f2cf45447f88c115626
Justin Handville <nanolith@gmail.com>
2023-08-16 19:51:26 -0400

    Converted unit tests to minunit.

--- test/doubly_linked_list/test_doubly_linked_list_insert_after.cpp
+++ test/doubly_linked_list/test_doubly_linked_list_insert_after.cpp
@@ -3,17 +3,16 @@
  *
  * Unit tests for doubly_linked_list_insert_after
  *
- * \copyright 2019-2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2019-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
 #include <vpr/allocator/malloc_allocator.h>
 #include <vpr/doubly_linked_list.h>
 
-/* DISABLED GTEST */
-#if 0
-class dll_insert_after_test : public ::testing::Test {
-protected:
-    void LocalSetUp(bool copy_on_insert)
+class dll_insert_after_test {
+public:
+    void localSetUp(bool copy_on_insert)
     {
         malloc_allocator_options_init(&alloc_opts);
         doubly_linked_list_options_init_status =
@@ -21,7 +20,7 @@ protected:
                 copy_on_insert, sizeof(int), false);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         if (VPR_STATUS_SUCCESS == doubly_linked_list_options_init_status)
         {
@@ -35,101 +34,109 @@ protected:
     doubly_linked_list_options_t options;
 };
 
-TEST_F(dll_insert_after_test, basic_test)
-{
-    LocalSetUp(true);
+TEST_SUITE(dll_insert_after_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    dll_insert_after_test fixture;
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
+}
+
+BEGIN_TEST_F(basic_test)
+    fixture.localSetUp(true);
 
     /* verify that options init succeeded. */
-    ASSERT_EQ(VPR_STATUS_SUCCESS, doubly_linked_list_options_init_status);
+    TEST_ASSERT(
+        VPR_STATUS_SUCCESS == fixture.doubly_linked_list_options_init_status);
 
     doubly_linked_list dll;
 
-    ASSERT_EQ(doubly_linked_list_init(&options, &dll), 0);
+    TEST_ASSERT(doubly_linked_list_init(&fixture.options, &dll) == 0);
 
     // insert some data
     int data = 356;
-    ASSERT_EQ(doubly_linked_list_insert_beginning(&dll, &data), 0);
+    TEST_ASSERT(doubly_linked_list_insert_beginning(&dll, &data) == 0);
 
     // the number of elements should be 1, with
     // the first and last pointers pointing to the same element
-    EXPECT_EQ(dll.elements, 1UL);
-    EXPECT_EQ(dll.first, dll.last);
+    TEST_EXPECT(dll.elements == 1UL);
+    TEST_EXPECT(dll.first == dll.last);
 
     // the next / prev pointers on the initial element should be set to NULL
-    EXPECT_EQ(dll.first->prev, nullptr);
-    EXPECT_EQ(dll.first->next, nullptr);
+    TEST_EXPECT(dll.first->prev == nullptr);
+    TEST_EXPECT(dll.first->next == nullptr);
 
     // and the data should be as expected
-    EXPECT_EQ(*(int*)(dll.first->data), data);
+    TEST_EXPECT(*(int*)(dll.first->data) == data);
 
     // insert something after the initial element
     int data2 = 205;
-    ASSERT_EQ(doubly_linked_list_insert_after(&dll, dll.first, &data2), 0);
-    EXPECT_EQ(dll.elements, 2UL);
-    EXPECT_EQ(*(int*)(dll.first->data), data);
-    EXPECT_EQ(*(int*)(dll.last->data), data2);
+    TEST_ASSERT(doubly_linked_list_insert_after(&dll, dll.first, &data2) == 0);
+    TEST_EXPECT(dll.elements == 2UL);
+    TEST_EXPECT(*(int*)(dll.first->data) == data);
+    TEST_EXPECT(*(int*)(dll.last->data) == data2);
 
     // test the links on each element
-    EXPECT_EQ(dll.first->prev, nullptr);
-    EXPECT_EQ(dll.first->next, dll.last);
-    EXPECT_EQ(dll.last->prev, dll.first);
-    EXPECT_EQ(dll.last->next, nullptr);
+    TEST_EXPECT(dll.first->prev == nullptr);
+    TEST_EXPECT(dll.first->next == dll.last);
+    TEST_EXPECT(dll.last->prev == dll.first);
+    TEST_EXPECT(dll.last->next == nullptr);
 
     //dispose of our list
     dispose(doubly_linked_list_disposable_handle(&dll));
-}
+END_TEST_F()
 
-TEST_F(dll_insert_after_test, with_copy_on_insert)
-{
-    LocalSetUp(true);
+BEGIN_TEST_F(with_copy_on_insert)
+    fixture.localSetUp(true);
 
     doubly_linked_list dll;
 
-    ASSERT_EQ(doubly_linked_list_init(&options, &dll), 0);
+    TEST_ASSERT(doubly_linked_list_init(&fixture.options, &dll) == 0);
 
     // insert some data
     int data = 356;
-    ASSERT_EQ(doubly_linked_list_insert_beginning(&dll, &data), 0);
+    TEST_ASSERT(doubly_linked_list_insert_beginning(&dll, &data) == 0);
 
     // insert something after the initial element
     int data2 = 205;
-    ASSERT_EQ(doubly_linked_list_insert_after(&dll, dll.first, &data2), 0);
+    TEST_ASSERT(doubly_linked_list_insert_after(&dll, dll.first, &data2) == 0);
 
     // test that the data was copied
     data++;
-    EXPECT_EQ(*(int*)(dll.first->data), data - 1);
+    TEST_EXPECT(*(int*)(dll.first->data) == data - 1);
 
     data2 += 10;
-    EXPECT_EQ(*(int*)(dll.last->data), data2 - 10);
+    TEST_EXPECT(*(int*)(dll.last->data) == data2 - 10);
 
     //dispose of our list
     dispose(doubly_linked_list_disposable_handle(&dll));
-}
+END_TEST_F()
 
-TEST_F(dll_insert_after_test, without_copy_on_insert)
-{
-    LocalSetUp(false);
+BEGIN_TEST_F(without_copy_on_insert)
+    fixture.localSetUp(false);
 
     doubly_linked_list dll;
 
-    ASSERT_EQ(doubly_linked_list_init(&options, &dll), 0);
+    TEST_ASSERT(doubly_linked_list_init(&fixture.options, &dll) == 0);
 
     // insert some data
     int data = 356;
-    ASSERT_EQ(doubly_linked_list_insert_beginning(&dll, &data), 0);
+    TEST_ASSERT(doubly_linked_list_insert_beginning(&dll, &data) == 0);
 
     // insert something after the initial element
     int data2 = 205;
-    ASSERT_EQ(doubly_linked_list_insert_after(&dll, dll.first, &data2), 0);
+    TEST_ASSERT(doubly_linked_list_insert_after(&dll, dll.first, &data2) == 0);
 
     // test that the data was NOT copied
     data++;
-    EXPECT_EQ(*(int*)(dll.first->data), data);
+    TEST_EXPECT(*(int*)(dll.first->data) == data);
 
     data2 += 10;
-    EXPECT_EQ(*(int*)(dll.last->data), data2);
+    TEST_EXPECT(*(int*)(dll.last->data) == data2);
 
     //dispose of our list
     dispose(doubly_linked_list_disposable_handle(&dll));
-}
-#endif
+END_TEST_F()
