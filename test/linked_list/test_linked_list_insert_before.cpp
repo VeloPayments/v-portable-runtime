56b9533f779c670f88a29d819b5dc4bd200fac84
Justin Handville <nanolith@gmail.com>
2023-08-17 12:21:40 -0400

    Converted unit tests to minunit.

--- test/linked_list/test_linked_list_insert_before.cpp
+++ test/linked_list/test_linked_list_insert_before.cpp
@@ -3,17 +3,16 @@
  *
  * Unit tests for linked_list_insert_before
  *
- * \copyright 2019-2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2019-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
 #include <vpr/allocator/malloc_allocator.h>
 #include <vpr/linked_list.h>
 
-/* DISABLED GTEST */
-#if 0
-class ll_insert_before_test : public ::testing::Test {
-protected:
-    void LocalSetUp(bool copy_on_insert)
+class ll_insert_before_test {
+public:
+    void localSetUp(bool copy_on_insert)
     {
         malloc_allocator_options_init(&alloc_opts);
         linked_list_options_init_status =
@@ -21,7 +20,7 @@ protected:
                 copy_on_insert, sizeof(long), false);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         if (VPR_STATUS_SUCCESS == linked_list_options_init_status)
         {
@@ -35,118 +34,124 @@ protected:
     linked_list_options_t options;
 };
 
-TEST_F(ll_insert_before_test, options_init)
-{
-    LocalSetUp(true);
-    ASSERT_EQ(VPR_STATUS_SUCCESS, linked_list_options_init_status);
+TEST_SUITE(ll_insert_before_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    ll_insert_before_test fixture;
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
 }
 
-TEST_F(ll_insert_before_test, basic_test)
-{
-    LocalSetUp(true);
+BEGIN_TEST_F(options_init)
+    fixture.localSetUp(true);
+    TEST_ASSERT(VPR_STATUS_SUCCESS == fixture.linked_list_options_init_status);
+END_TEST_F()
+
+BEGIN_TEST_F(basic_test)
+    fixture.localSetUp(true);
 
     linked_list_t ll;
 
-    ASSERT_EQ(linked_list_init(&options, &ll), 0);
+    TEST_ASSERT(linked_list_init(&fixture.options, &ll) == 0);
 
     long data = 2023988L;
 
-    ASSERT_EQ(linked_list_insert_end(&ll, &data), 0);
+    TEST_ASSERT(linked_list_insert_end(&ll, &data) == 0);
 
     // the number of elements should be 1, with
     // the first and last pointers pointing this element
-    EXPECT_EQ(ll.elements, 1UL);
-    EXPECT_EQ(ll.first, ll.last);
+    TEST_EXPECT(ll.elements == 1UL);
+    TEST_EXPECT(ll.first == ll.last);
 
     // the pointer on the initial element should be set to NULL
-    EXPECT_EQ(ll.first->next, nullptr);
+    TEST_EXPECT(ll.first->next == nullptr);
 
     // and the data should be as expected
-    EXPECT_EQ(*(long*)(ll.first->data), 2023988L);
+    TEST_EXPECT(*(long*)(ll.first->data) == 2023988L);
 
     // insert an element before the initial one
     long data2 = 205L;
-    ASSERT_EQ(linked_list_insert_before(&ll, ll.first, &data2), 0);
+    TEST_ASSERT(linked_list_insert_before(&ll, ll.first, &data2) == 0);
 
     // the number of elements should be 2, with the first element being the
     // second data item and the last element being the first data item
-    EXPECT_EQ(ll.elements, 2UL);
-    EXPECT_EQ(*(long*)(ll.first->data), data2);
-    EXPECT_EQ(*(long*)(ll.last->data), data);
+    TEST_EXPECT(ll.elements == 2UL);
+    TEST_EXPECT(*(long*)(ll.first->data) == data2);
+    TEST_EXPECT(*(long*)(ll.last->data) == data);
 
     // test the links on each element
-    EXPECT_EQ(ll.first->next, ll.last);
-    EXPECT_EQ(ll.last->next, nullptr);
+    TEST_EXPECT(ll.first->next == ll.last);
+    TEST_EXPECT(ll.last->next == nullptr);
 
     // add one more element, before the initial element
     // now the order should be data3 -> data2 -> data
     long data3 = -992L;
-    ASSERT_EQ(linked_list_insert_before(&ll, ll.first, &data3), 0);
-    EXPECT_EQ(ll.elements, 3UL);
-    EXPECT_EQ(*(long*)(ll.first->data), data3);
-    EXPECT_EQ(*(long*)(ll.first->next->data), data2);
-    EXPECT_EQ(*(long*)(ll.first->next->next->data), data);
-    EXPECT_EQ(*(long*)(ll.last->data), data);
+    TEST_ASSERT(linked_list_insert_before(&ll, ll.first, &data3) == 0);
+    TEST_EXPECT(ll.elements == 3UL);
+    TEST_EXPECT(*(long*)(ll.first->data) == data3);
+    TEST_EXPECT(*(long*)(ll.first->next->data) == data2);
+    TEST_EXPECT(*(long*)(ll.first->next->next->data) == data);
+    TEST_EXPECT(*(long*)(ll.last->data) == data);
 
     // test some pointers
-    EXPECT_EQ(ll.first->next->next, ll.last);
-    EXPECT_EQ(ll.last->next, nullptr);
+    TEST_EXPECT(ll.first->next->next == ll.last);
+    TEST_EXPECT(ll.last->next == nullptr);
 
     //dispose of our list
     dispose(linked_list_disposable_handle(&ll));
-}
+END_TEST_F()
 
-TEST_F(ll_insert_before_test, with_copy_on_insert)
-{
-    LocalSetUp(true);
+BEGIN_TEST_F(with_copy_on_insert)
+    fixture.localSetUp(true);
 
     linked_list_t ll;
 
-    ASSERT_EQ(linked_list_init(&options, &ll), 0);
+    TEST_ASSERT(linked_list_init(&fixture.options, &ll) == 0);
 
     // insert some data
     long data = 356;
-    ASSERT_EQ(linked_list_insert_end(&ll, &data), 0);
+    TEST_ASSERT(linked_list_insert_end(&ll, &data) == 0);
 
     // insert something after the initial element
     long data2 = 205;
-    ASSERT_EQ(linked_list_insert_after(&ll, ll.first, &data2), 0);
+    TEST_ASSERT(linked_list_insert_after(&ll, ll.first, &data2) == 0);
 
     // test that the data was copied
     data++;
-    EXPECT_EQ(*(long*)(ll.first->data), data - 1);
+    TEST_EXPECT(*(long*)(ll.first->data) == data - 1);
 
     data2 += 10;
-    EXPECT_EQ(*(long*)(ll.last->data), data2 - 10);
+    TEST_EXPECT(*(long*)(ll.last->data) == data2 - 10);
 
     //dispose of our list
     dispose(linked_list_disposable_handle(&ll));
-}
+END_TEST_F()
 
-TEST_F(ll_insert_before_test, without_copy_on_insert)
-{
-    LocalSetUp(false);
+BEGIN_TEST_F(without_copy_on_insert)
+    fixture.localSetUp(false);
 
     linked_list_t ll;
 
-    ASSERT_EQ(linked_list_init(&options, &ll), 0);
+    TEST_ASSERT(linked_list_init(&fixture.options, &ll) == 0);
 
     // insert some data
     long data = 356;
-    ASSERT_EQ(linked_list_insert_end(&ll, &data), 0);
+    TEST_ASSERT(linked_list_insert_end(&ll, &data) == 0);
 
     // insert something after the initial element
     long data2 = 205;
-    ASSERT_EQ(linked_list_insert_after(&ll, ll.first, &data2), 0);
+    TEST_ASSERT(linked_list_insert_after(&ll, ll.first, &data2) == 0);
 
     // test that the data was NOT copied
     data++;
-    EXPECT_EQ(*(long*)(ll.first->data), data);
+    TEST_EXPECT(*(long*)(ll.first->data) == data);
 
     data2 += 10;
-    EXPECT_EQ(*(long*)(ll.last->data), data2);
+    TEST_EXPECT(*(long*)(ll.last->data) == data2);
 
     //dispose of our list
     dispose(linked_list_disposable_handle(&ll));
-}
-#endif
+END_TEST_F()
