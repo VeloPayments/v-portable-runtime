36b22ac95949e9f2e4c54576189786ed3f679dbf
Justin Handville <nanolith@gmail.com>
2023-08-16 21:15:41 -0400

    Converted unit tests to minunit.

--- test/dynamic_array/test_dynamic_array_grow.cpp
+++ test/dynamic_array/test_dynamic_array_grow.cpp
@@ -3,18 +3,17 @@
  *
  * Unit tests for dynamic_array_grow.
  *
- * \copyright 2017-2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
 #include <vpr/allocator/malloc_allocator.h>
 #include <vpr/compare.h>
 #include <vpr/dynamic_array.h>
 
-/* DISABLED GTEST */
-#if 0
-class dynamic_array_grow_test : public ::testing::Test {
-protected:
-    void SetUp() override
+class dynamic_array_grow_test {
+public:
+    void setUp()
     {
         malloc_allocator_options_init(&alloc_opts);
         dynamic_array_options_init_status =
@@ -22,7 +21,7 @@ protected:
                 &options, &alloc_opts, sizeof(int), &compare_int);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         if (VPR_STATUS_SUCCESS == dynamic_array_options_init_status)
         {
@@ -36,109 +35,120 @@ protected:
     dynamic_array_options_t options;
 };
 
+TEST_SUITE(dynamic_array_grow_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    dynamic_array_grow_test fixture; \
+    fixture.setUp();
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
+}
+
 /**
  * dynamic_array_options_init should succeed.
  */
-TEST_F(dynamic_array_grow_test, options_init)
-{
-    ASSERT_EQ(VPR_STATUS_SUCCESS, dynamic_array_options_init_status);
-}
+BEGIN_TEST_F(options_init)
+    TEST_ASSERT(
+        VPR_STATUS_SUCCESS == fixture.dynamic_array_options_init_status);
+END_TEST_F()
 
 /**
  * Create an empty array, and grow the reserved size by 1.
  */
-TEST_F(dynamic_array_grow_test, basic_test)
-{
+BEGIN_TEST_F(basic_test)
     dynamic_array_t array;
 
     //we should be able to create a dynamic array
-    ASSERT_EQ(VPR_STATUS_SUCCESS,
-        dynamic_array_init(&options, &array, 1, 0, NULL));
+    TEST_ASSERT(
+        VPR_STATUS_SUCCESS
+            == dynamic_array_init(&fixture.options, &array, 1, 0, NULL));
 
     //we should have a reserved space of 1
-    ASSERT_EQ((size_t)1, array.reserved_elements);
+    TEST_ASSERT((size_t)1 == array.reserved_elements);
     //there should be no element instances
-    ASSERT_EQ((size_t)0, array.elements);
+    TEST_ASSERT((size_t)0 == array.elements);
 
     //grow the array by 1
-    EXPECT_EQ(0, dynamic_array_grow(&array, 2));
+    TEST_EXPECT(0 == dynamic_array_grow(&array, 2));
 
     //the reserved size should be 2
-    EXPECT_EQ((size_t)2, array.reserved_elements);
+    TEST_EXPECT((size_t)2 == array.reserved_elements);
     //the instance size should still be 0
-    EXPECT_EQ((size_t)0, array.elements);
+    TEST_EXPECT((size_t)0 == array.elements);
 
     //dispose the array
     dispose(dynamic_array_disposable_handle(&array));
-}
+END_TEST_F()
 
 /**
  * Attempting to shrink the array should fail.
  */
-TEST_F(dynamic_array_grow_test, shrink_failure)
-{
+BEGIN_TEST_F(shrink_failure)
     dynamic_array_t array;
 
     //we should be able to create a dynamic array
-    ASSERT_EQ(VPR_STATUS_SUCCESS,
-        dynamic_array_init(&options, &array, 5, 0, NULL));
+    TEST_ASSERT(
+        VPR_STATUS_SUCCESS
+            == dynamic_array_init(&fixture.options, &array, 5, 0, NULL));
 
     //we should have a reserved space of 5
-    ASSERT_EQ((size_t)5, array.reserved_elements);
+    TEST_ASSERT((size_t)5 == array.reserved_elements);
     //there should be no element instances
-    ASSERT_EQ((size_t)0, array.elements);
+    TEST_ASSERT((size_t)0 == array.elements);
 
     //Attempt to shrink the array to 2 elements.  This should fail.
-    ASSERT_NE(0, dynamic_array_grow(&array, 2));
+    TEST_ASSERT(0 != dynamic_array_grow(&array, 2));
 
     //the reserved size should still be 5
-    EXPECT_EQ((size_t)5, array.reserved_elements);
+    TEST_EXPECT((size_t)5 == array.reserved_elements);
     //the instance size should still be 0
-    EXPECT_EQ((size_t)0, array.elements);
+    TEST_EXPECT((size_t)0 == array.elements);
 
     //dispose the array
     dispose(dynamic_array_disposable_handle(&array));
-}
+END_TEST_F()
 
 /**
  * Growing the array should copy instantianted elements.
  */
-TEST_F(dynamic_array_grow_test, grow_copy)
-{
+BEGIN_TEST_F(grow_copy)
     int SEVENTEEN = 17;
     dynamic_array_t array;
 
     //we should be able to create a dynamic array
-    ASSERT_EQ(VPR_STATUS_SUCCESS,
-        dynamic_array_init(&options, &array, 5, 5, &SEVENTEEN));
+    TEST_ASSERT(
+        VPR_STATUS_SUCCESS
+            == dynamic_array_init(&fixture.options, &array, 5, 5, &SEVENTEEN));
 
     //we should have a reserved space of 5
-    ASSERT_EQ((size_t)5, array.reserved_elements);
+    TEST_ASSERT((size_t)5 == array.reserved_elements);
     //All five elements should be instantiated
-    ASSERT_EQ((size_t)5, array.elements);
+    TEST_ASSERT((size_t)5 == array.elements);
 
     //all elements should be set
     int* intArr = (int*)array.array;
     for (int i = 0; i < 5; ++i)
     {
-        ASSERT_EQ(SEVENTEEN, intArr[i]);
+        TEST_ASSERT(SEVENTEEN == intArr[i]);
     }
 
     //Grow the array to 100 elements.
-    ASSERT_EQ(0, dynamic_array_grow(&array, 100));
+    TEST_ASSERT(0 == dynamic_array_grow(&array, 100));
 
     //the reserved size should be 100
-    EXPECT_EQ((size_t)100, array.reserved_elements);
+    TEST_EXPECT((size_t)100 == array.reserved_elements);
     //the instance size should still be 5
-    EXPECT_EQ((size_t)5, array.elements);
+    TEST_EXPECT((size_t)5 == array.elements);
     //all elements should be set
     intArr = (int*)array.array;
     for (int i = 0; i < 5; ++i)
     {
-        ASSERT_EQ(SEVENTEEN, intArr[i]);
+        TEST_ASSERT(SEVENTEEN == intArr[i]);
     }
 
     //dispose the array
     dispose(dynamic_array_disposable_handle(&array));
-}
-#endif
+END_TEST_F()
